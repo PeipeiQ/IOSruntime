010d84152d329f91537689d0b3ce7433a0334edb
reinhardt.q <reinhardt.q@vipshop.com>
2018-06-13 15:42:20 +0800

    init

---
+++ IOSruntime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IOSruntime
+//
+//  Created by peipei on 2018/6/13.
+//  Copyright © 2018年 peipei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ IOSruntime/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  IOSruntime
+//
+//  Created by peipei on 2018/6/13.
+//  Copyright © 2018年 peipei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
